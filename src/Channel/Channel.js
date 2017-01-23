5e0e6f5d18b722e23c8b66f04ac408c5236ce230
Alexis Wurth <alexis.wurth57@gmail.com>
2017-01-23 17:06:29 +0100

    Removed useless methods

--- src/Channel/Channel.js
+++ src/Channel/Channel.js
@@ -2,7 +2,6 @@ import app from '../app'
 
 app.factory('Channel', ['$resource', 'api', function ($resource, api) {
   return $resource(api.url + '/channels/:id', {id: '@_id'}, {
-    getAll: {isArray: true},
     update: {method: 'PUT'}
   })
 }])
