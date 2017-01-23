4d416cf820de0bd3bebb61f60c73021be64242bb
Alexis Wurth <alexis.wurth57@gmail.com>
2017-01-23 17:17:57 +0100

    Renamed templates

--- src/config.js
+++ src/config.js
@@ -29,11 +29,11 @@ app.config(['$httpProvider', '$routeProvider', 'api', function ($httpProvider, $
       controller: 'AuthCtrl'
     })
     .when('/channels', {
-      templateUrl: 'partials/create-channel.html',
+      templateUrl: 'partials/channel-creation.html',
       controller: 'ChannelCreationCtrl'
     })
     .when('/channels/:id', {
-      templateUrl: 'partials/channel.html',
+      templateUrl: 'partials/channel-view.html',
       controller: 'ChannelShowCtrl'
     })
     .otherwise({redirectTo: '/'})
