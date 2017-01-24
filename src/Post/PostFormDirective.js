2c8f8c3e19776119ba99f91c0251952d427cb911
Alexis Wurth <alexis.wurth57@gmail.com>
2017-01-24 17:50:03 +0100

    Reorganized code

--- src/Post/PostFormDirective.js
+++ src/Post/PostFormDirective.js
@@ -11,19 +11,21 @@ app.directive('postForm', ['Post', function (Post) {
     link: ($scope, element, attrs) => {
       $scope.onKeyup = (event) => {
         let field = $(event.target)
-        field.css('overflow', 'hidden')
-        field.css('height', 0)
-        field.css('height', field.prop('scrollHeight') + 'px')
 
         if (event.key === 'Enter') {
           field.val('')
           Post.save({ channel_id: $scope.channelId }, $scope.post, (response) => {
-            $scope.posts = Post.query({channel_id: $scope.channelId})
-            $('html, body').animate({
-              scrollTop: field.offset().top
-            }, 'fast')
+            $scope.posts = Post.query({channel_id: $scope.channelId}, () => {
+              $('html, body').animate({
+                scrollTop: $('#comments_bottom').offset().top
+              }, 'fast')
+            })
           })
         }
+
+        field.css('overflow', 'hidden')
+        field.css('height', 0)
+        field.css('height', field.prop('scrollHeight') + 'px')
       }
     }
   }
