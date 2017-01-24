1dcfcc34070eecd797d660ff81e8b66cc245280d
Alexis Wurth <alexis.wurth57@gmail.com>
2017-01-24 17:07:02 +0100

    Scroll to bottom when entering a channel

--- src/Channel/ChannelShowController.js
+++ src/Channel/ChannelShowController.js
@@ -1,9 +1,13 @@
-import { app } from '../app'
+import { app, $ } from '../app'
 
 app.controller('ChannelShowCtrl', ['$scope', '$routeParams', 'Channel', 'Post', 'Member', function ($scope, $routeParams, Channel, Post, Member) {
   $scope.channel = Channel.get({id: $routeParams.id})
   $scope.posts = Post.query({channel_id: $routeParams.id})
-  $scope.members = Member.query()
+  $scope.members = Member.query(() => {
+    $('html, body').animate({
+      scrollTop: $('#post_message').offset().top
+    }, 'fast')
+  })
 
   $scope.getPostMember = (post) => {
     for (let member of $scope.members) {
