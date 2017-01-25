309d7f5919fd5eae06f3dbfbdf83ccb38d9bdda7
Alexis Wurth <alexis.wurth57@gmail.com>
2017-01-25 20:59:40 +0100

    Revert "Use html5 mode for routing"
    This reverts commit 9440ec76f2e33e58b2f50d116119fddc7547264e.

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 import { app } from './app'
 
-app.config(['$httpProvider', '$routeProvider', '$locationProvider', 'api', function ($httpProvider, $routeProvider, $locationProvider, api) {
+app.config(['$httpProvider', '$routeProvider', 'api', function ($httpProvider, $routeProvider, api) {
   $httpProvider.defaults.headers.common.Authorization = 'Token token=' + api.key
 
   $httpProvider.interceptors.push(['TokenService', function (TokenService) {
@@ -15,7 +15,6 @@ app.config(['$httpProvider', '$routeProvider', '$locationProvider', 'api', funct
     }
   }])
 
-  $locationProvider.html5Mode(true)
   $routeProvider
     .when('/', {
       templateUrl: 'partials/home.html',
