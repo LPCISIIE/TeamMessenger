475ca4f18795555462083b0a2d53e4a2916c7039
Alexis Wurth <alexis.wurth57@gmail.com>
2017-01-27 20:21:37 +0100

    Added webpack plugin

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -4,6 +4,7 @@ let config = require('./config')
 let FriendlyErrors = require('friendly-errors-webpack-plugin')
 
 webpackConfig.plugins = webpackConfig.plugins.concat([
+  new webpack.optimize.OccurenceOrderPlugin(),
   new webpack.HotModuleReplacementPlugin(),
   new webpack.NoErrorsPlugin(),
   new FriendlyErrors({
