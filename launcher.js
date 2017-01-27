e7f4f39adcb2e02a0416a8a62ff0b6e7ef143f6b
xchopin <xchopin54@gmail.com>
2017-01-28 00:41:14 +0100

    Tried to import angular but it does not work

--- launcher.js
+++ launcher.js
@@ -1,4 +1,15 @@
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.$ = undefined;
+
+const _angular = require('angular');
+
 const electron = require('electron')
+
 const app = electron.app
 
 const BrowserWindow = electron.BrowserWindow
@@ -52,5 +63,3 @@ app.on('activate', function () {
   }
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
