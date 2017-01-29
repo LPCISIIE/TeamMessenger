2d978918cdb4a65a9441ac2e29e7ea30cca87679
Alexis Wurth <alexis.wurth57@gmail.com>
2017-01-29 18:00:27 +0100

    Edit and delete channels

--- src/Channel/ChannelViewController.js
+++ src/Channel/ChannelViewController.js
@@ -1,10 +1,27 @@
+import { $ } from '../main'
 
-export default function ChannelViewController ($scope, $routeParams, Channel, Post) {
-  Channel.get({id: $routeParams.id}, (channel) => {
+export default function ChannelViewController ($scope, $routeParams, $location, Channel, Post) {
+  $('.ui.dropdown').dropdown()
+
+  Channel.get({ id: $routeParams.id }, (channel) => {
     $scope.channel = channel
   })
 
-  Post.query({channel_id: $routeParams.id}, (posts) => {
+  Post.query({ channel_id: $routeParams.id }, (posts) => {
     $scope.posts = posts
   })
+
+  $scope.editForm = () => {
+    $scope.editChannel = true
+  }
+
+  $scope.edit = () => {
+    Channel.update({ id: $scope.channel._id }, $scope.channel, () => {
+      $scope.editChannel = false
+    })
+  }
+
+  $scope.delete = () => {
+    Channel.delete({ id: $scope.channel._id }, () => $location.path('/'))
+  }
 }
