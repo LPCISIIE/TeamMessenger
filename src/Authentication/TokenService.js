2690f260a12e8e20900d3a2c247ce21f7058f277
Alexis Wurth <alexis.wurth57@gmail.com>
2017-02-01 15:20:07 +0100

    Converted TokenService into a class

--- src/Authentication/TokenService.js
+++ src/Authentication/TokenService.js
@@ -1,26 +1,32 @@
 
-export default function TokenService () {
-  this.setToken = (token) => {
-    window.localStorage.setItem('token_date', Date.now())
-    window.localStorage.setItem('token', token)
+export default class TokenService {
+  constructor ($window) {
+    this.$window = $window
   }
 
-  this.getToken = () => {
-    let date = window.localStorage.getItem('token_date')
+  setToken (token) {
+    this.$window.localStorage.setItem('token_date', Date.now())
+    this.$window.localStorage.setItem('token', token)
+  }
+
+  getToken () {
+    let date = this.$window.localStorage.getItem('token_date')
 
     // On teste la validité du token
     // Si la dernière requête était il y a plus de 30 min (1800000 ms)
     // on supprime le token, sinon on actualise la date
     if (date && Date.now() - parseInt(date) > 1800000) {
-      window.localStorage.removeItem('token')
+      this.$window.localStorage.removeItem('token')
     } else {
-      window.localStorage.setItem('token_date', Date.now())
+      this.$window.localStorage.setItem('token_date', Date.now())
     }
 
-    return window.localStorage.getItem('token')
+    return this.$window.localStorage.getItem('token')
   }
 
-  this.removeToken = () => {
-    window.localStorage.removeItem('token')
+  removeToken () {
+    this.$window.localStorage.removeItem('token')
   }
 }
+
+TokenService.$inject = ['$window']
